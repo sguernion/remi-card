6253d2f90807bcef5daf4e839e38480dc37413e2
sguernion <gsylvain35@gmail.com>
2026-01-10 23:17:22 +0100

    add conf for release

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,11 +3,12 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
 import terser from '@rollup/plugin-terser';
+import copy from 'rollup-plugin-copy';
 
 export default {
   input: 'src/remi-card.ts',
   output: {
-    file: 'remi-card.js',
+    file: 'dist/remi-card.js',
     format: 'es',
     sourcemap: true,
     inlineDynamicImports: true,
@@ -21,6 +22,11 @@ export default {
       declaration: false,
     }),
     json(),
+    // copy assets into dist so they are available at dist/assets
+    copy({
+      targets: [{ src: 'assets', dest: 'dist' }],
+      verbose: true,
+    }),
     terser({
       compress: {
         drop_console: false,
